6b91147f895b50491c5372f74b3db752ffe6ce72
Yashes <134478676+itsYashes@users.noreply.github.com>
2024-09-12 13:55:45 +0000

    Conversion

---
+++ 03conversion.js
@@ -0,0 +1,21 @@
+// let score = null
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+
+// let isLoggedIn = "yashes"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+let setNumber = 33
+
+let stringNumber = String(setNumber)
+console.log(typeof stringNumber);
+
