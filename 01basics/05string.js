eeab3f4a4be170d8a73b3b0f0f6b9e5e8480b15a
Yashes <134478676+itsYashes@users.noreply.github.com>
2024-12-01 17:42:00 +0000

    String

---
+++ 01basics/05string.js
@@ -0,0 +1,34 @@
+const name = "YashasRavindra";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Yashes-game-id");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf("e"));
+
+// const newString = gameName.substring(8, 4);
+// console.log(newString);
+
+// const anotherName = gameName.slice(-8, 4);
+// console.log(anotherName);
+
+const newStringOne = "   yashes   "
+// console.log(newStringOne.trim());
+// console.log(newStringOne);
+
+const url = "https://hithesh.com/yash%20ravindra"
+
+// console.log(url.replace('%20', '-'))
+
+console.log(url.includes('yashes'));
+
+console.log(gameName.split('-'));
