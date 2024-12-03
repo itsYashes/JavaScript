7159b22e5c6266130e9611aabc2cffddf57db9c1
Yashes <134478676+itsYashes@users.noreply.github.com>
2024-12-03 12:48:12 +0000

    For in

---
+++ 05Loops/04Four.js
@@ -0,0 +1,28 @@
+// For in
+
+const myObj = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swith by Apple'
+}
+
+for (const key in myObj) {
+    // console.log(`${key} shortcut is for ${myObj[key]}`);      
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('IN', "India")
+// map.set('US', "United States of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
